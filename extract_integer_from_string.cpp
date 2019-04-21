e58873b22b2fffa602f57ce18223717e332ed444
Saeem Osman <saeem.osman@gmail.com>
2019-04-21 16:24:31 +0600

    extract_integer_from_string.cpp

---
+++ extract_integer_from_string.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include <vector>
+#define len 100
+
+vector <int> findInteger( char* a){
+vector<int> v;
+for(int i = 0; a[i]; i++){
+
+    int k = (int) a[i];
+    if( k >= 47 && k<= 58){
+        v.push_back(k);
+    }
+
+}
+return v;
+}
+int main(){
+    vector<int> v;
+    vector<int>:: iterator it;
+    char a[len];
+    cout<<"Enter a string: ";
+    gets(a);
+
+    v = findInteger(a);
+    it = v.begin();
+    for(it; it != v.end(); it++){
+        cout<<(char)*it<<" ";
+    }
+}
