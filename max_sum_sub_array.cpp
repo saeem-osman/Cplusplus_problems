e3efee5038abffa413e205a69694c04984dc7561
Saeem Osman <saeem.osman@gmail.com>
2019-04-26 01:07:03 +0600

    max_sum_sub_array_leetcode

---
+++ max_sum_sub_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = INT_MIN;
+        int sum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i]>=sum+nums[i])
+                sum = nums[i];
+            else
+                sum += nums[i];
+            if(sum > max_sum)
+                max_sum = sum;
+        }
+        return max_sum;
+    }
+};
